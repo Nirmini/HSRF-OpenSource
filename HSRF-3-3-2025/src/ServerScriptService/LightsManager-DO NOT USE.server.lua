f8cc27d1a55373de7d7e5eef1f9ae3c45c57422b
Frost/West7014 <87557086+thatWest7014@users.noreply.github.com>
2025-03-06 04:18:06 +0000

    Update LightsManager-DO NOT USE.server.lua

--- HSRF-3-3-2025/src/ServerScriptService/LightsManager-DO NOT USE.server.lua
+++ HSRF-3-3-2025/src/ServerScriptService/LightsManager-DO NOT USE.server.lua
@@ -1,16 +1,16 @@
 local ServerScriptService = game:GetService("ServerScriptService")
 local function disableLights()
 	for _, light in ipairs(game.Workspace:GetDescendants()) do
-		if light:IsA("PointLight") or light:IsA("SpotLight") or light:IsA("SurfaceLight") then
-			light.Enabled = false
-		end
+	if light:IsA("PointLight") or light:IsA("SpotLight") or light:IsA("SurfaceLight") then
+		light.Enabled = false
+	end
 	end
 end
 local function enableLights()
 	for _, light in ipairs(game.Workspace:GetDescendants()) do
-		if light:IsA("PointLight") or light:IsA("SpotLight") or light:IsA("SurfaceLight") then
-			light.Enabled = true
-		end
+	if light:IsA("PointLight") or light:IsA("SpotLight") or light:IsA("SurfaceLight") then
+		light.Enabled = true
+	end
 	end
 end
 ServerScriptService.Reactor.Startup.Event:Connect(function()
