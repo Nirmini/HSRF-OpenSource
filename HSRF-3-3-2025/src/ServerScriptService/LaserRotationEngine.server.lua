7351641af63cdeeeb08d40b5cbba4bf16d30d9ab
Frost/West7014 <87557086+thatWest7014@users.noreply.github.com>
2025-03-06 04:17:41 +0000

    Update LaserRotationEngine.server.lua

--- HSRF-3-3-2025/src/ServerScriptService/LaserRotationEngine.server.lua
+++ HSRF-3-3-2025/src/ServerScriptService/LaserRotationEngine.server.lua
@@ -10,15 +10,15 @@ local laserModels = {
 }
 local function rotateModels()
 	while true do
-		wait(1/60)
-		for _, modelName in ipairs(laserModels) do
-			local model = game.Workspace["SL2-RO2"]:FindFirstChild(modelName)
-			if model then
-				local head = model:FindFirstChild('Head')
-				if head then
-					head.CFrame = head.CFrame * CFrame.Angles(math.rad(-3), 0, 0)
-				end
-			end
+	wait(1/60)
+	for _, modelName in ipairs(laserModels) do
+		local model = game.Workspace["SL2-RO2"]:FindFirstChild(modelName)
+		if model then
+			local head = model:FindFirstChild('Head')
+		if head then
+			head.CFrame = head.CFrame * CFrame.Angles(math.rad(-3), 0, 0)
+		end
+		end
 		end
 	end
 end
